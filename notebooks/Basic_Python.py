5d724773a4dc270bd12dafc7ba3f2ae92dee79af
Piti Ongmongkolkul <piti118@gmail.com>
2012-12-16 08:48:22 -0800

    Add notebooks directory

---
+++ notebooks/Basic_Python.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=3>
+
+# Hello World
+
+# <codecell>
+
+print 'Hello world'
+
+# <codecell>
+
+#ipython automatically show representation of the return value of the last command
+1+1
+
+# <headingcell level=3>
+
+# Data Type(Usual Stuff)
+
+# <codecell>
+
+#integer
+x = 1
+print x
+
+# <codecell>
+
+#float
+y = 1.2
+print y
+print x+y #can use variable from previous execution too
+
+# <codecell>
+
+
+# <codecell>
+
+
+# <codecell>
+
+
+# <codecell>
+
+
